39985c96692a0eb94f44c2d9aaef366a4abf2ce6
robert-brumleve <brumlevr@oregonstate.edu>
2026-07-08 11:44:32 +0900

    Milestone 1.2 Validate Deck and some Card metadata

--- src/lib/validators/deckValidator.ts
+++ src/lib/validators/deckValidator.ts
@@ -1,4 +1,5 @@
 import { Deck } from "../../types/Deck";
+import { Card } from "../../types/Card";
 import { ValidationResult } from "../../types/ValidationResult";
 
 export function validateDeck(deck: Deck): ValidationResult {
@@ -6,37 +7,43 @@ export function validateDeck(deck: Deck): ValidationResult {
 
     //*** Validate Deck ***
     // Check if ID exists and isn't blank.
-    if (!validateID(deck.id))
-        errors.push(`Deck "${deck.name}": ID is required.`)
+    if (isNullOrEmptyString(deck.id))
+        errors.push(`Deck Name "${deck.name}": ID is required.`)
     // Check if Name exists and isn't blank.
-    // enter code here
+    if (isNullOrEmptyString(deck.name))
+        errors.push(`Deck ID "${deck.id}": Name is required.`)
     // Check if Cards array exists.
-    // enter code here
+    if (!Array.isArray(deck.cards))
+        errors.push(`Deck Name "${deck.name}": Cards array is required.`)
 
     //*** Validate Card ***
-    // Check if ID exists.
-    // enter code here
-    // Check if Order exists.
-    // enter code here
-    // Check if Type exists.
-    // enter code here
-    // Check if Front exists.
-    // enter code here
-    // Check if Back exists.
-    // enter code here
-
+    for (const i in deck.cards) {
+        const card: Card = deck.cards[i]
+        // Check if ID exists.
+        if (isNullOrEmptyString(card.id))
+            errors.push(`Card Order "${card.order}": ID is required.`)
+        // Check if Order exists.
+        if (!Number.isInteger(card.order))
+            errors.push(`Deck ID "${card.id}": Order is required.`)
+        // Check if Type exists.
+        // enter code here
+        // Check if Front exists.
+        // enter code here
+        // Check if Back exists.
+        // enter code here
+    }
     return {
         valid: errors.length === 0,
         errors
     };
 }
 
-function validateID(id: string): boolean {
-    //catch undefined and null
+function isNullOrEmptyString(id: string | null | undefined): boolean {
+    // catch undefined and null
     if (id == null)
-        return false;
+        return true;
     // catch empty string and IDs with only white spaces
     if (id.trim() === "")
-        return false;
-    return true
+        return true;
+    return false
 }
